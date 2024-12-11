0af712c3c0f461e4c903a32ac3442738b2344ce8
Isabelly-O <Isabelly.C.J.Oliveira@gmail.com>
2024-12-10 22:36:23 -0300

    rota

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,21 @@
-import { Router } from 'express';
+import { Router, Request, Response, RequestHandler } from 'express';
 import userController from '../controllers/userController';
+import authService from '../services/authService';
+
+const handlerLogin: RequestHandler = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { error, token } = await authService.authenticateUser(req.body.email, req.body.senha);
+
+        if (error) {
+            res.status(401).json({ error });
+        }
+
+        res.status(200).json({ token });
+    } catch (err) {
+        res.status(500).json({ error: 'server error' });
+    }
+};
+
 class Routes {
     static define(router: Router): Router { 
 
@@ -12,6 +28,8 @@ class Routes {
         router.put('/user/:id', userController.updateUser);
 
         router.delete('/user/:id', userController.deleteUser);
+
+        router.post('/login', handlerLogin);
 
         return router;
     }
